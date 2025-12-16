511ccc8cc213b16fef86dd734bc2197371560517
KINGOFXD807 <auk8400@gmail.com>
2025-12-16 22:08:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -112,8 +112,8 @@ const {
   console.log('Plugins installed successful ✅')
   console.log('Bot connected to whatsapp ✅')
   
-  let up = `*YOUR BOT AWAIS MD ACTIVE NOW ENJOY♥️*\n\n*PREFIX:* ${prefix}`;
-    conn.sendMessage(conn.user.id, { image: { url: `https://github.com/Awais-star-a11y/TESTING-REPO/raw/refs/heads/main/IMG-20250409-WA0093.jpg` }, caption: up })
+  let up = `*YOUR BOT AWAIS-MD-V5 ACTIVE NOW ENJOY♥️*\n\n*PREFIX:* ${prefix}`;
+    conn.sendMessage(conn.user.id, { image: { url: `https://files.catbox.moe/04ux9p.jpg` }, caption: up })
   }
   })
   conn.ev.on('creds.update', saveCreds)
